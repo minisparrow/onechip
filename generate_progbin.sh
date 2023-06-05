20e9eb143b4b538a7ead0f0a711db61ec51a9985
minisparrow <melchior_faith@163.com>
2023-06-05 22:17:51 +0800

    reorg dirs

--- generate_progbin.sh
+++ generate_progbin.sh
@@ -1,2 +1,2 @@
-riscv64-unknown-elf-gcc hello.c -march=rv64g -ffreestanding -nostdlib -static -Wl,-Ttext=0 -O2
+riscv64-unknown-elf-gcc apps/hello.c -march=rv64g -ffreestanding -nostdlib -static -Wl,-Ttext=0 -O2
 llvm-objcopy-10 -j .text -O binary a.out prog.bin
