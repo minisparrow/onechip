3731d2aaa7d8cbd9209c5b11f660aeac92d1c46f
minisparrow <melchior_faith@163.com>
2023-06-11 19:20:15 +0800

    single cycle cpu

---
+++ TOYCPU/src/main/scala/03_lw/Top.scala
@@ -0,0 +1,18 @@
+package lw
+
+import chisel3._
+
+class Top extends Module {
+  val io = IO(new Bundle {
+    val exit = Output(Bool())
+  })
+  val core = Module(new Core())
+  val memory = Module(new Memory())
+  core.io.imem <> memory.io.imem
+  core.io.dmem <> memory.io.dmem
+  io.exit := core.io.exit
+}
+
+object TOYCPU extends App {
+  (new chisel3.stage.ChiselStage).emitVerilog (new Top, args)
+}
