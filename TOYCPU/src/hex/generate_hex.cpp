8bdee52ada0ed24155900505f1a77caaf4e728cf
minisparrow <melchior_faith@163.com>
2023-07-02 15:06:25 +0800

    Add buffer generator for ai accelerator

---
+++ TOYCPU/src/hex/generate_hex.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+const int ARRAY_SIZE = 64;
+void saveToHex(uint8_t* buf, const char* hexFileName) {
+     FILE *hex_file = fopen(hexFileName, "w");
+     if (hex_file == NULL)
+     {
+         printf("Error opening file\n");
+     }
+     for (int i = 0; i < ARRAY_SIZE; i++)
+     {
+         char hex_data[5];
+         sprintf(hex_data, "%02X", buf[i]); // convert to hex string
+         fprintf(hex_file, "%s\n", hex_data); // write to file
+     }
+     fclose(hex_file);
+}
+
+int main()
+{
+    uint8_t abuf[64];
+    uint8_t bbuf[64];
+    uint8_t cbuf[64];
+
+    for(int i = 0; i < ARRAY_SIZE; i++) {
+        abuf[i] = i;
+        bbuf[i] = i;
+        cbuf[i] = 0;
+    }
+    saveToHex(abuf, "abuf.hex");
+    saveToHex(bbuf, "bbuf.hex");
+    saveToHex(cbuf, "cbuf.hex");
+    return 0;
+}
